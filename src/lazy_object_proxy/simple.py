50f5aa901dcaadfbae80ac435005d4c9689c093a
Ionel Cristian Mărieș <contact@ionelmc.ro>
2014-12-28 06:45:48 +0200

    Refactor some utility code to a compat module.

--- src/lazy_object_proxy/simple.py
+++ src/lazy_object_proxy/simple.py
@@ -1,14 +1,8 @@
-from functools import partial
-import sys
 import operator
 
-PY2 = sys.version_info[0] == 2
-PY3 = sys.version_info[0] == 3
-
-
-def with_metaclass(meta, *bases):
-    """Create a base class with a metaclass."""
-    return meta("NewBase", bases, {})
+from .compat import PY2
+from .compat import PY3
+from .compat import with_metaclass
 
 
 class cached_property(object):
--- src/lazy_object_proxy/slots.py
+++ src/lazy_object_proxy/slots.py
@@ -1,13 +1,8 @@
-import sys
 import operator
 
-PY2 = sys.version_info[0] == 2
-PY3 = sys.version_info[0] == 3
-
-
-def with_metaclass(meta, *bases):
-    """Create a base class with a metaclass."""
-    return meta("NewBase", bases, {})
+from .compat import PY2
+from .compat import PY3
+from .compat import with_metaclass
 
 
 class _ProxyMethods(object):
