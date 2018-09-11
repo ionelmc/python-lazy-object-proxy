063d18fb3aee7ffe8c6515b7cb1cdb2a5fe7290a
hugovk <hugovk@users.noreply.github.com>
2018-09-11 22:50:41 +0300

    Add python_requires to help pip

--- setup.py
+++ setup.py
@@ -74,6 +74,7 @@ setup(
     py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
     include_package_data=True,
     zip_safe=False,
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
     classifiers=[
         # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
         'Development Status :: 5 - Production/Stable',
