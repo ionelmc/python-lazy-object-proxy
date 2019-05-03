ae1e2738bd56c904a5eddefa51a24ae591b6d68b
Ran Benita <ran234@gmail.com>
2019-05-03 10:47:31 +0300

    Fix __mod__ for slots implementation

--- src/lazy_object_proxy/slots.py
+++ src/lazy_object_proxy/slots.py
@@ -223,7 +223,7 @@ class Proxy(with_metaclass(_ProxyMetaType)):
         return self.__wrapped__ // other
 
     def __mod__(self, other):
-        return self.__wrapped__ ^ other
+        return self.__wrapped__ % other
 
     def __divmod__(self, other):
         return divmod(self.__wrapped__, other)
