8448c233ff12bd1aa11a7297009ddce0608e62f4
Ionel Cristian Mărieș <contact@ionelmc.ro>
2022-10-26 11:10:08 +0300

    Fix pytest deprecation.

--- tests/conftest.py
+++ tests/conftest.py
@@ -20,7 +20,7 @@ def lop_loader():
                     from lazy_object_proxy.cext import Proxy
                 except ImportError:
                     if PYPY:
-                        pytest.skip(msg="C Extension not available.")
+                        pytest.skip(reason="C Extension not available.")
                     else:
                         raise
             elif name == "objproxies":
@@ -37,11 +37,15 @@ def lop_loader():
     return load_implementation
 
 
-@pytest.fixture(scope="session", params=[
-    "slots", "cext",
-    "simple",
-    # "external-django", "external-objproxies"
-])
+@pytest.fixture(
+    scope="session",
+    params=[
+        "slots",
+        "cext",
+        "simple",
+        # "external-django", "external-objproxies"
+    ],
+)
 def lop_implementation(request, lop_loader):
     return lop_loader(request.param)
 
@@ -49,10 +53,10 @@ def lop_implementation(request, lop_loader):
 @pytest.fixture(scope="session", params=[True, False], ids=['subclassed', 'normal'])
 def lop_subclass(request, lop_implementation):
     if request.param:
+
         class submod(lop_implementation):
             subclass = True
-            Proxy = type("SubclassOf_" + lop_implementation.Proxy.__name__,
-                         (lop_implementation.Proxy,), {})
+            Proxy = type("SubclassOf_" + lop_implementation.Proxy.__name__, (lop_implementation.Proxy,), {})
 
         return submod
     else:
@@ -62,13 +66,12 @@ def lop_subclass(request, lop_implementation):
 @pytest.fixture(scope="function")
 def lop(request, lop_subclass):
     if request.node.get_closest_marker('xfail_subclass'):
-        request.applymarker(pytest.mark.xfail(
-            reason="This test can't work because subclassing disables certain "
-                   "features like __doc__ and __module__ proxying."
-        ))
+        request.applymarker(
+            pytest.mark.xfail(
+                reason="This test can't work because subclassing disables certain " "features like __doc__ and __module__ proxying."
+            )
+        )
     if request.node.get_closest_marker('xfail_simple'):
-        request.applymarker(pytest.mark.xfail(
-            reason="The lazy_object_proxy.simple.Proxy has some limitations."
-        ))
+        request.applymarker(pytest.mark.xfail(reason="The lazy_object_proxy.simple.Proxy has some limitations."))
 
     return lop_subclass
